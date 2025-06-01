baf0795bf2b4231e138426d1461a14e3daa49c95
Harold Yesid Gonzalez Mancipe <xh4rz@hotmail.com>
2025-06-01 03:07:21 -0500

    Chapter 3: Build a screen

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,13 +1,19 @@
-import { Link } from 'expo-router';
-import { StyleSheet, Text, View } from 'react-native';
+import Button from '@/components/Button';
+import ImageViewer from '@/components/ImageViewer';
+import { StyleSheet, View } from 'react-native';
+
+const PlaceholderImage = require('@/assets/images/background-image.png');
 
 export default function Index() {
 	return (
 		<View style={styles.container}>
-			<Text style={styles.text}>Home screen</Text>
-			<Link href="/about" style={styles.button}>
-				Go to About screen
-			</Link>
+			<View style={styles.imageContainer}>
+				<ImageViewer imgSource={PlaceholderImage} />
+			</View>
+			<View style={styles.footerContainer}>
+				<Button theme="primary" label="Choose a photo" />
+				<Button label="Use this photo" />
+			</View>
 		</View>
 	);
 }
@@ -16,15 +22,14 @@ const styles = StyleSheet.create({
 	container: {
 		flex: 1,
 		backgroundColor: '#25292E',
-		alignItems: 'center',
-		justifyContent: 'center'
+		alignItems: 'center'
 	},
-	text: {
-		color: '#fff'
+	imageContainer: {
+		flex: 1,
+		paddingTop: 28
 	},
-	button: {
-		fontSize: 20,
-		textDecorationLine: 'underline',
-		color: '#fff'
+	footerContainer: {
+		flex: 1 / 3,
+		alignItems: 'center'
 	}
 });
