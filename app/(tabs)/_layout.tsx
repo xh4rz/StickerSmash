d381575df159141a9e20de967effa0baf8eb456c
Harold Yesid Gonzalez Mancipe <xh4rz@hotmail.com>
2025-06-01 02:31:34 -0500

    Chapter 2: Add navigation

---
+++ app/(tabs)/_layout.tsx
@@ -0,0 +1,49 @@
+import { Ionicons } from '@expo/vector-icons';
+import { Tabs } from 'expo-router';
+
+export default function TabLayout() {
+	return (
+		<Tabs
+			screenOptions={{
+				tabBarActiveTintColor: '#FFD33D',
+				headerStyle: {
+					backgroundColor: '#25292E'
+				},
+				headerShadowVisible: false,
+				headerTintColor: '#FFF',
+				tabBarStyle: {
+					backgroundColor: '#25292E'
+				}
+			}}
+		>
+			<Tabs.Screen
+				name="index"
+				options={{
+					title: 'Home',
+					tabBarIcon: ({ color, focused }) => (
+						<Ionicons
+							name={focused ? 'home-sharp' : 'home-outline'}
+							color={color}
+							size={24}
+						/>
+					)
+				}}
+			/>
+			<Tabs.Screen
+				name="about"
+				options={{
+					title: 'About',
+					tabBarIcon: ({ color, focused }) => (
+						<Ionicons
+							name={
+								focused ? 'information-circle' : 'information-circle-outline'
+							}
+							color={color}
+							size={24}
+						/>
+					)
+				}}
+			/>
+		</Tabs>
+	);
+}
